16fa48b8cb137f9056e0741ee9b6e3c1f36c9110
Meng Le Zhang <mzhang@hotmail.co.uk>
2023-01-23 12:27:03 +0000

    Issues (#4)
    * lookup from lsoa to ttwa done using official ons lookup now
    * got di and simplifed geoms 
    
    only penzance remains an issue
    
    * renamed files

---
+++ 02 join data.R
@@ -0,0 +1,82 @@
+##  02 UK (well England and Wales) social frontiers
+##  README: Every file needed is committed in the repo except the lsoa file shp which
+##  has to be manually placed in the /data folder
+
+## 0. Preamble ----
+sapply(
+  c(
+    'tidyverse',
+    'tmap',
+    'sf',
+    'socialFrontiers'
+  ),
+  library, character.only = T
+)
+
+
+# inpit/output ------------------------------------------------------------
+
+
+## 0. Start with master lookup 
+ttwa2011_lookup<- readRDS('temp/ttwa and lsoa lookup.rds')
+
+##  1. lsoa gis file and join to make cob2011_sf ----
+
+lsoa11_sf <-
+  readRDS(
+    'temp/lsoa 2011 sf.rds'
+  )
+
+
+##  2. Get the pop files from census ----
+
+cob2011_df <-
+  readRDS(
+    'temp/country of birth uk 2011 oa lsoa.rds'
+  )
+
+cob2011_df <-
+  cob2011_df %>%
+  filter(
+    zoneType == 'lsoa2011'
+  )
+
+## join data 
+
+master_sf <-
+  lsoa11_sf %>% 
+  left_join(ttwa2011_lookup %>% select(-objectid)) %>% # objectid isn't a joining var
+  left_join(
+    cob2011_df,
+    by = c('lsoa11cd' = 'zoneID')
+  )
+
+master_sf %>% summary
+
+## data cleaning 
+master_sf <-
+  master_sf %>%
+  rename(
+    zoneID = lsoa11cd,
+    zoneNm = lsoa11nm
+  ) %>%
+  transmute(
+    zoneID,
+    zoneNm,
+    ttwa11cd,
+    ttwa11nm,
+    zoneType,
+    allResidents,
+    ukBorn,
+    nonUKBorn
+  )
+
+
+##  end. Save the file! --------------------------------------
+
+master_sf %>% 
+  saveRDS(
+    file = 'temp/cleaned cob lsoa ttwa.rds'
+  )
+
+####
--- misc.R
+++ misc.R
@@ -1,4 +1,15 @@
 # 0x: extract areal level data for dashboard app
+## Goals:
+## 1. simplify objects for quicker loading: https://www.r-bloggers.com/2021/03/simplifying-geospatial-features-in-r-with-sf-and-rmapshaper/
+##  2. Convert to long lat ("EPSG:4326")
+
+
+library(rmapshaper) ## needed for simplifying
+
+# inputs ------------------------------------------------------------------
+
+longlat_crs <- "EPSG:4326"
+
 
 ## Extract 
 cob_ttwa_sf <-
@@ -12,13 +23,24 @@ cob_ttwa_sf <-
   cob_ttwa_sf %>%
   transmute(
     zoneID,
-    zoneNm = lsoa11nm,
+    zoneNm,
     allResidents,
     ukBorn,
     nonUKBorn,
     ttwa = paste(ttwa11nm, '(2011)'),
     geometry
   )
+
+
+# simplify the geometries and transform to longlat -------------------------------------------------
+cob_ttwa_sf <- 
+  cob_ttwa_sf %>% st_transform(crs = "EPSG:4326")
+
+cob_ttwa_sf <-
+  ms_simplify(cob_ttwa_sf, keep = 0.001,
+              keep_shapes = FALSE)
+## shrunk from 15mb to 2mb
+# cob_ttwa_sf %>% tmap::qtm()
 
 
 ## Save ot 
@@ -28,5 +50,49 @@ cob_ttwa_sf %>%
 
 # ttwa layer --------------------------------------------------------------
 
-ttwa2011_sf <- readRDS('temp/ttwa 2011.rds')
-ttwa2011_sf %>% saveRDS('output/ttwa 2011 layer.rds')
+##  3. Load ttwa 
+ttwa11_sf <-
+  readRDS(
+    'temp/ttwa 2011.rds'
+  )
+
+## add other stats 
+
+ttwa_summary <-
+  readRDS('temp/05 summary stats.rds')
+
+ttwa11_sf <-
+  ttwa11_sf %>% 
+  left_join(
+    ttwa_summary
+  )
+
+##  get england and wales only
+ttwa11_sf <-
+  ttwa11_sf %>%
+  mutate(
+    country = ttwa11cd %>% substr(1,1)
+  ) %>%
+  filter(
+    country %in% c('E', 'W')
+  )
+## better name
+ttwa11_sf <-
+  ttwa11_sf %>%
+  mutate(
+    ttwa = paste(ttwa11nm, '(2011)')
+  )
+
+
+
+
+## convert crs and simplify 
+ttwa11_sf <- 
+  ttwa11_sf %>% st_transform(crs = "EPSG:4326")
+
+ttwa11_sf <-
+  ms_simplify(ttwa11_sf, keep = 0.001,
+              keep_shapes = FALSE)
+
+### save 
+ttwa11_sf %>% saveRDS('output/ttwa 2011 layer.rds')
