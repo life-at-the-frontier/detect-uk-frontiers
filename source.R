6f1a2c853665a54cca00e104f23e9902ac2f5955
Meng <mzhang@hotmail.co.uk>
2023-06-02 11:48:43 +0100

    added option for local library (kinda works for me but not dan)

--- source.R
+++ source.R
@@ -1,5 +1,12 @@
 ##  common-00-utils
 
+## Local library ----------------------------------
+# ## For best compatibility use windows and a local folder containing all the packs
+# ##  download from google drive
+# local_lib <- '4.0' ## if it's in the local R project folder -- change path whatever
+# .libPaths(local_lib)
+# .libPaths()
+
 ## 0. Preamble ----
 sapply(
   c(
@@ -8,6 +15,7 @@ sapply(
     'sf',
     'socialFrontiers'
   ),
-  library, character.only = T
+  library, 
+  character.only = T
 )
 
